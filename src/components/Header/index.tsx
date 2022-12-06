bf6753615e3070c5507fa838c45149fd3e98aef9
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-12-06 13:59:10 -0300

    component: Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,34 @@
+import { Flex, Grid, Image } from "@chakra-ui/react";
+
+export default function () {
+  return (
+    <Flex
+      bg="white"
+      w="100%"
+      as="header"
+      mx="auto"
+      px="1rem"
+      h={["50px", "100px"]}
+      align="center"
+      justify="center"
+    >
+      <Grid
+        h="100%"
+        mx="auto"
+        w="100%"
+        maxW="1170px"
+        alignItems="center"
+        templateColumns="repeat(3, 1f)"
+        justifyContent="center"
+      >
+        <Image
+          w={["81px", "184px"]}
+          src="/Logo.svg"
+          alt="Um avião voando sobre o nome da marca worldtrip"
+          justifySelf="center"
+          gridColumn="2"
+        />
+      </Grid>
+    </Flex>
+  );
+}

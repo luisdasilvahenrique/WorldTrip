43af4c4a181338a7e55b55fd99f1b74db36caa46
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-12-16 00:16:19 -0300

    components: continent and ContinentBanner

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,14 @@
-import { Flex, Grid, Image } from "@chakra-ui/react";
+import { Flex, Grid, Icon, Image } from "@chakra-ui/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import { RiArrowLeftSLine } from 'react-icons/ri';
 
 export default function () {
+
+  const { asPath } = useRouter();
+  const IsNotHomePage = asPath !== "/";
+
   return (
     <Flex
       bg="white"
@@ -13,14 +21,22 @@ export default function () {
       justify="center"
     >
       <Grid
-        h="100%"
+         h="100%"
         mx="auto"
         w="100%"
-        maxW="1170px"
+        maxW="1160px"
         alignItems="center"
-        templateColumns="repeat(3, 1f)"
+        templateColumns="repeat(3, 1fr)"
         justifyContent="center"
+        alignSelf="start"
       >
+
+    { IsNotHomePage && (
+      <Link href="/">
+        <Icon as={RiArrowLeftSLine} fontSize={[20, 40]} justifySelf="start"/>
+      </Link>
+    )}
+
         <Image
           w={["81px", "184px"]}
           src="/Logo.svg"
